a8b1a1aa05148727a1d3c88767a564580a05c697
dofoyo <dofoyo@sina.cn>
2018-12-28 09:55:19 +0000

    init

---
+++ src/main/java/com/rhb/dcode/DcodeApplication.java
@@ -0,0 +1,14 @@
+package com.rhb.dcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcodeApplication.class, args);
+	}
+
+}
+
